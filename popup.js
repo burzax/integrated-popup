e8abf669e341b9de89ed8ddf5d246174efd720de
Grzegorz Burzyński <brikson@gmail.com>
2017-08-20 12:10:35 +0200

    initial-setup

---
+++ popup.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#quoteGrabber').click(function(){pasteSelection();});
+});
+function pasteSelection() {
+  chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
+  function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, {method: "wysylamZapytanie"}, 
+    function(response){
+      console.log('dostaje odpowiedz');
+      console.log(response);
+      var text = document.getElementById('text'); 
+      text.innerHTML = response.data;
+    });
+  });
+}
